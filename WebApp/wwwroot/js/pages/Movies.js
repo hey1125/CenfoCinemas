d38402dc032e036fcc1796912dedb966a148ff57
hey1125 <valeriocdaniel@gmail.com>
2025-07-23 17:50:26 -0600

    listo

--- WebApp/wwwroot/js/pages/Movies.js
+++ WebApp/wwwroot/js/pages/Movies.js
@@ -5,14 +5,27 @@
     this.InitView = function () {
         console.log("Movie init view --> ok");
         this.LoadTable();
+
+        $("#btnCreate").click(function () {
+            var vc = new MoviesViewController();
+            vc.Create();
+        });
+        $("#btnUpdate").click(function () {
+            var vc = new MoviesViewController();
+            vc.Update();
+        });
+        $("#btnDelete").click(function () {
+            var vc = new MoviesViewController();
+            vc.Delete();
+        });
     };
 
     this.LoadTable = function () {
         var ca = new ControlActions();
         var service = this.ApiEndpointName + "/RetriveAll";
         var urlService = ca.GetUrlApiService(service);
-        var columns = [];
 
+        var columns = [];
         columns.push({ 'data': 'id' });
         columns.push({ 'data': 'title' });
         columns.push({ 'data': 'description' });
@@ -20,16 +33,89 @@
         columns.push({ 'data': 'genre' });
         columns.push({ 'data': 'director' });
 
-        $("#tblMovies").dataTable({
+        $('#tblMovies').DataTable({
             "ajax": {
-                url: urlService,
+                "url": urlService,
                 "dataSrc": ""
             },
             columns: columns
         });
+
+        $('#tblMovies tbody').on('click', 'tr', function () {
+            var row = $(this).closest('tr');
+            var movieDTO = $('#tblMovies').DataTable().row(row).data();
+
+            $("#txtId").val(movieDTO.id);
+            $("#txtTitle").val(movieDTO.title);
+            $("#txtDescription").val(movieDTO.description);
+            $("#txtReleaseDate").val(movieDTO.releaseDate.split("T")[0]);
+            $("#txtGenre").val(movieDTO.genre);
+            $("#txtDirector").val(movieDTO.director);
+        });
+    };
+
+    this.Create = function () {
+        var movieDTO = {};
+        movieDTO.id = 0;
+        movieDTO.created = "2025-01-01";
+        movieDTO.updated = "2025-01-01!";
+
+        movieDTO.title = $("#txtTitle").val();
+        movieDTO.description = $("#txtDescription").val();
+        movieDTO.releaseDate = $("#txtReleaseDate").val();
+        movieDTO.genre = $("#txtGenre").val();
+        movieDTO.director = $("#txtDirector").val();
+
+        var ca = new ControlActions();
+        var urlService = this.ApiEndpointName + "/Create";
+
+        ca.PostToAPI(urlService, movieDTO, function () {
+            $('#tblMovies').DataTable().ajax.reload();
+        });
+    };
+
+    this.Update = function () {
+        var movieDTO = {};
+        movieDTO.id = $("#txtId").val();
+        movieDTO.created = "2025-01-01";
+        movieDTO.updated = "2025-01-01!";
+
+        movieDTO.title = $("#txtTitle").val();
+        movieDTO.description = $("#txtDescription").val();
+        movieDTO.releaseDate = $("#txtReleaseDate").val();
+        movieDTO.genre = $("#txtGenre").val();
+        movieDTO.director = $("#txtDirector").val();
+
+        var ca = new ControlActions();
+        var urlService = this.ApiEndpointName + "/Update";
+
+        ca.PutToAPI(urlService, movieDTO, function () {
+            $('#tblMovies').DataTable().ajax.reload();
+        });
+    };
+
+    this.Delete = function () {
+        var movieDTO = {};
+        movieDTO.id = $("#txtId").val();
+        movieDTO.created = "2025-01-01";
+        movieDTO.updated = "2025-01-01!";
+
+        movieDTO.title = $("#txtTitle").val();
+        movieDTO.description = $("#txtDescription").val();
+        movieDTO.releaseDate = $("#txtReleaseDate").val();
+        movieDTO.genre = $("#txtGenre").val();
+        movieDTO.director = $("#txtDirector").val();
+
+        var ca = new ControlActions();
+        var urlService = this.ApiEndpointName + "/Delete";
+
+        ca.DeleteToAPI(urlService, movieDTO, function () {
+            $('#tblMovies').DataTable().ajax.reload();
+        });
     };
 }
 
+// Documento listo
 $(document).ready(function () {
     var vc = new MoviesViewController();
     vc.InitView();
